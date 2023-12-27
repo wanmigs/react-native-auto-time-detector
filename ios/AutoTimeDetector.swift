b101033ac7f10d279d386c6f4feacfe2da68d2ff
Juan Miguel Torres <wanmigs@gmail.com>
2023-12-27 21:41:50 +0800

    chore: initial commit

---
+++ ios/AutoTimeDetector.swift
@@ -0,0 +1,8 @@
+@objc(AutoTimeDetector)
+class AutoTimeDetector: NSObject {
+
+  @objc(multiply:withB:withResolver:withRejecter:)
+  func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
+    resolve(a*b)
+  }
+}
